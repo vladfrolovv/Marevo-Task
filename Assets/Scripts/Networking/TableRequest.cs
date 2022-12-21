5c83a769411a63037d3b1b846e78f5be32315d18
Vlad Frolov <73vladfrolov73@gmail.com>
2022-12-21 13:34:47 +0200

    google sheets request

---
+++ Assets/Scripts/Networking/TableRequest.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TableRequest : MonoBehaviour {
+  private const string googleAPIKey = "AIzaSyCo5tiU7G2EzQXJWBu09l0P5bDKlKawJ8k";
+  private const string spreadsheetID = "1Wj75QfY2F8PkNCTMYvOsL-FxYia2mdGvQITVti1xHMk";
+  private const string googleSpreadsheetLink = "https://sheets.googleapis.com/v4/spreadsheets/";
+  private const string keyPath = "/values/Sheet1?key=";
+
+  private void Start() {
+    StartCoroutine(GetTableJson());
+  }
+  
+  private IEnumerator GetTableJson() {
+    string link = (googleSpreadsheetLink + spreadsheetID + keyPath + googleAPIKey);
+    using UnityWebRequest request = UnityWebRequest.Get(link);
+    yield return request.SendWebRequest();
+    if (request.result != UnityWebRequest.Result.Success) {
+      // show error || send another request
+      Debug.Log("ERROR: " + request.responseCode);
+      yield break;
+    }
+    
+    Debug.Log("Received JSON" + request.downloadHandler.text);   
+    // parse with classes
+  }
+}
