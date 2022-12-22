0c25501c87470c3c3bebba6b9c3774a172d2d828
Vlad Frolov <73vladfrolov73@gmail.com>
2022-12-22 12:10:57 +0200

    get/set texture in ar mode

--- Assets/Scripts/AR/OnARModeOpen.cs
+++ Assets/Scripts/AR/OnARModeOpen.cs
@@ -1,17 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class OnARModeOpen : MonoBehaviour {
   [SerializeField] private GameObject parallelepipedPrefab;
+
+  private GameObject parallelepiped;
   private void CreateParallelepiped() {
-    GameObject parallelepiped = Instantiate(parallelepipedPrefab, Vector3.zero, Quaternion.identity);
+    parallelepiped = Instantiate(parallelepipedPrefab, Vector3.zero, Quaternion.identity);
     parallelepiped.transform.localScale = DimensionsMenu.lastDimensions / 100;
     parallelepiped.transform.localPosition += new Vector3(0, parallelepiped.transform.localPosition.y / 2, 0);
-    Debug.Log("Texture link: " + DimensionsMenu.lastObject.textureLink);
+    StartCoroutine(GetTextureRequest());
   }
 
   private void Start() {
     CreateParallelepiped();
   }
+
+  private IEnumerator GetTextureRequest() {
+    using UnityWebRequest request = UnityWebRequestTexture.GetTexture(DimensionsMenu.lastObject.previewLink);
+    yield return request.SendWebRequest();
+    
+    if (request.result != UnityWebRequest.Result.Success) {
+      Debug.Log(request.responseCode + " " + request.error + " With Link: " + DimensionsMenu.lastObject.previewLink);
+    } else {
+      Texture previewTexture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+      parallelepiped.GetComponent<Renderer>().material.mainTexture = previewTexture;
+    }
+  }
+  
 }
